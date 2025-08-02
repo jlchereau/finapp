0dd76f52610b88f7193f9bfebc8aa27ef9b5b9fb
Jacques L. Chereau <jlchereau@users.noreply.github.com>
2025-08-02 08:49:37 +0200

    Fixed pylint warnings

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,8 @@
+"""The reflex (rx) app configuration."""
+
 import reflex as rx
 
-config = rx.Config(
+config = rx.Config(  # pylint: disable=not-callable
     app_name="app",
     plugins=[
         rx.plugins.SitemapPlugin(),
