9cd7c76aafc8c53a93f63292788652340d8563ce
Jacques L. Chereau <jlchereau@users.noreply.github.com>
2025-08-10 11:19:37 +0200

    Fixed lint issue

--- app/components/headlessui.py
+++ app/components/headlessui.py
@@ -8,7 +8,8 @@ See:
 This base class is mainly responsible for loading the react libraries
 according to Reflex documentation about React wrappers for components
 that do not render server side:
-    - https://reflex.dev/docs/wrapping-react/library-and-tags/#wrapping-a-dynamic-component
+    - https://reflex.dev/docs/wrapping-react/library-and-tags/
+    #wrapping-a-dynamic-component
     - https://reflex.dev/docs/wrapping-react/more-wrapping-examples#react-leaflet
 """
 
