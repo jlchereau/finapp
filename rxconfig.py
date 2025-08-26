8b328b05b566a60733c44c5359359e05f8cbe356
Jacques L. Chereau <jlchereau@users.noreply.github.com>
2025-08-26 17:15:56 +0200

    Added placeholder for API_URL as reminder

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,8 @@ import reflex as rx
 
 config = rx.Config(  # pylint: disable=not-callable
     app_name="app",
+    # https://reflex.dev/docs/hosting/self-hosting/
+    # api_url="http://localhost:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
